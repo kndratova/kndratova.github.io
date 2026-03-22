b7a700729a00faf462c663ccd985f86cc9c6caae
kndratova <katiakondratova@gmail.com>
2026-03-22 19:40:15 +0300

    cleanup

--- public/js/login.js
+++
@@ -1,3 +0,0 @@
-import {handleLoginFormSubmit} from './auth.js';
-
-document.getElementById('loginForm').addEventListener('submit', handleLoginFormSubmit);
--- src/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    PORT: 63342,
-    DB_HOST: 'localhost',
-    DB_USER: 'root',
-    DB_PASSWORD: '19012002',
-    DB_NAME: 'melagrano_db',
-    SALT_ROUNDS: 10,
-    CHARSET: 'utf8',
-};
